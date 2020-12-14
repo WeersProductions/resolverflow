2f1be8867f577be1f1eacad20310ea3f08668969
Floris <weersproductions@gmail.com>
2020-12-14 12:53:10 +0100

    Update number_of_tags

--- features/number_of_tags.py
+++ features/number_of_tags.py
@@ -1,2 +1,9 @@
-def number_of_tags(tags):
-    return len(set(tags))
+# TODO: we should probably also use this file to check whether the tags contain platform and/or programming language.
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+def number_of_tags_df():
+    df = spark.read.parquet("/user/***REMOVED***/StackOverflow/Posts.parquet").select(["_Id","_Tags"]).dropna()
+    # Split tags.
+    df.show()
