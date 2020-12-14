5d60963379e142f2a244886a1fe378f9540f1f78
Frank Stapel <frank.5voor9@gmail.com>
2020-12-14 10:55:21 +0100

    Initial features

--- convertDataSet.py
+++ convertDataSet.py
@@ -3,10 +3,11 @@ from pyspark.sql.functions import explode
 
 spark = SparkSession.builder.getOrCreate()
 
-def Convert(source, destination, row_element):
+
+def convert(source, destination, row_element):
     df = spark.read.format("xml").options(rowTag=row_element).load(source)
     df.write.parquet(destination)
 
 
 # To do in pyspark, start with: pyspark --packages com.databricks:spark-xml_2.11:0.11.0
-Convert("Badges.xml", "Badges.parquet", "row")
+convert("Badges.xml", "Badges.parquet", "row")
---
+++ features/feature_number_of_tags.py
@@ -0,0 +1,2 @@
+def feature_number_of_tags(tags):
+    return len(set(tags))
