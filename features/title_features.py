53c76002863e5e6cbce90b15d4cd6222729727ab
Floris <weersproductions@gmail.com>
2020-12-16 10:09:59 +0100

    Add title features

---
+++ features/title_features.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, length
+
+spark = SparkSession.builder.getOrCreate()
+
+def title_features_df():
+    df = spark.read.parquet("/user/***REMOVED***/StackOverflow/Posts.parquet") \
+        .select(["_Id","_Title"]) \
+        .dropna() \
+        .withColumn('contains_questionmark', col("_Title").contains('?')) \
+        .withColumn('title_length', length(col("_Title")))
--- features/title_length.py
+++
@@ -1,2 +0,0 @@
-def title_length(title):
-    return len(title)
