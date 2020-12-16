7d0ba7bb13b10f87dcdfa27e5a5d8d0ee12277ad
Karel van Klink <karel@deob.us>
2020-12-16 12:08:04 +0100

    add post_score.py that returns the score of a post per id

---
+++ features/post_score.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+
+def post_score(spark):
+    """
+    Return a dataframe that only contains post ids and their score
+    :return: dataframe
+    """
+
+    df = spark.read.parquet('/user/***REMOVED***/StackOverflow/Posts.parquet') \
+        .select(['_Id', '_Score']) \
+        .dropna()
+
+    return df
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.getOrCreate()
+    post_score(spark).show()
