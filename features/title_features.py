bf00b905bf51b8f3ed83d700a4812c9ab1bb41a3
Floris <weersproductions@gmail.com>
2020-12-16 10:54:38 +0100

    Add run_all

--- features/title_features.py
+++ features/title_features.py
@@ -1,13 +1,16 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, length
 
-spark = SparkSession.builder.getOrCreate()
-
-def title_features_df():
+def title_features_df(spark):
     df = spark.read.parquet("/user/***REMOVED***/StackOverflow/Posts.parquet") \
-        .select(["_Id","_Title"]) \
+        .select(["_Id", "_Title"]) \
         .dropna() \
         .withColumn('contains_questionmark', col("_Title").contains('?')) \
         .withColumn('title_length', length(col("_Title")))
 
     return df
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.getOrCreate()
+    title_features_df(spark).show()
