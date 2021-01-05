573c5fef7e6c98d1a87cac723461708512d1da67
Floris <weersproductions@gmail.com>
2021-01-05 16:17:18 +0100

    Add question_amount

--- features/user_features.py
+++ features/user_features.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, to_timestamp
+from pyspark.sql.functions import col, to_timestamp, when, sum
 from pyspark.sql.types import LongType
 
 
@@ -19,12 +19,26 @@ def user_age_df(spark):
 
 
 def user_question_amount(spark):
+    """
+    Calculate the amount of questions a user has made before a post is created.
+    """
     df_posts = spark.read.parquet("/user/***REMOVED***/StackOverflow/Posts.parquet") \
-        .select(["_Id", "_OwnerUserId"])
-    df_post_count = df_posts.select(col("_OwnerUserId").alias("UserId")).groupBy("UserId").count()
-    result = df_posts.join(df_post_count, df_posts._OwnerUserId == df_post_count.UserId).select(col("_Id"), col("count").alias("posts_amount"))
+        .select(["_Id", "_OwnerUserId", "_CreationDate"]) \
+        .withColumn('CreationTimestamp', to_timestamp(col("_CreationDate")).cast(LongType())) \
+        .drop("_CreationDate")
+    # Renaming columns for self join.
+    df_post_count = df_posts
+    for c in df_posts.columns:
+        df_post_count = df_post_count if c == '_OwnerUserId' else df_post_count.withColumnRenamed(c, 'x_{cl}'.format(cl=c))
+
+    result = df_posts.join(df_post_count, '_OwnerUserId')
+    # Count the amount of posts before this post was made.
+    result = result.withColumn('posts_amount', when(col('CreationTimestamp') < col('x_CreationTimestamp'), 1).otherwise(0)).groupBy(['_Id', 'CreationTimestamp']).agg(sum('posts_amount').alias('posts_amount'))
+    result = result.drop("CreationTimestamp")
+    return result
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.getOrCreate()
-    user_age_df(spark).show()
+    # user_age_df(spark).show()
+    user_question_amount(spark).show()
