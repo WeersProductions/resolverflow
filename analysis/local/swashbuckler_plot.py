1de73003f025d0d1dfed7cfc1cf430f834a36f8f
Karel van Klink <karel@deob.us>
2021-01-18 12:20:58 +0100

    add z-score and reintroduce the stacks in swashbuckler_plot.py

--- analysis/local/swashbuckler_plot.py
+++ analysis/local/swashbuckler_plot.py
@@ -1,11 +1,12 @@
 import os
 import pickle
-from itertools import izip
 
 import matplotlib.pyplot as plt
+from scipy import stats
 from tqdm import tqdm
 
-ABSOLUTE = False
+ABSOLUTE = True
+STACKED = True
 PERCENTAGES = True
 
 if __name__ == "__main__":
@@ -15,7 +16,8 @@ if __name__ == "__main__":
     for pickle_file in tqdm(os.listdir(pickle_path)):
 
         # Resolved files only, we will load unresolved a few lines later (manually)
-        if pickle_file[-8] == '1':  # and pickle_file == 'output_#tags_1.pickle':
+        # TODO: remove comment on next line
+        if pickle_file[-8] == '1':  # and pickle_file != 'output_#characters_1.pickle':
             print('Plotting ' + pickle_file[:-9] + '...')
 
             resolved_data_points = pickle.load(open(pickle_path + pickle_file, "rb"))
@@ -27,13 +29,18 @@ if __name__ == "__main__":
             # counts: [[indices], [values]]
 
             # Change data format into {index: count} for both resolved and unresolved
-            # TODO: don't use tolist() loser
-            resolved_dict = dict(izip(resolved_counts[0].tolist(), resolved_counts[1].tolist()))
-            unresolved_dict = dict(izip(unresolved_counts[0].tolist(), unresolved_counts[1].tolist()))
+            resolved_dict = dict(zip(resolved_counts[0].tolist(), resolved_counts[1].tolist()))
+            unresolved_dict = dict(zip(unresolved_counts[0].tolist(), unresolved_counts[1].tolist()))
 
             # Create a unique list of all data points, to be used as x-ticks
             x_points = filter(lambda elem: elem is not None,
                               list(set(resolved_counts[0].tolist() + unresolved_counts[0].tolist())))
+
+            # Remove outliers to the right, keep any values down to x = 0
+            z_scores = stats.zscore(x_points)
+            x_points = filter(lambda e: e[1] < 1.5, zip(x_points, z_scores))
+            filtered_z_scores = [x[1] for x in x_points]
+            x_points = [x[0] for x in x_points]
 
             # Build y arrays with 0 as default value, such that they are in the same order (for stacking)
             y_resolved = []
@@ -42,12 +49,12 @@ if __name__ == "__main__":
                 y_resolved.append(resolved_dict.get(x, 0))
                 y_unresolved.append(unresolved_dict.get(x, 0))
 
-            if None in unresolved_counts or None in resolved_counts:
-                print(x_points)
-                print(y_resolved)
-                print(y_unresolved)
-                print('There is a None count in here somewhere above, ensure everything is in order!\n'
-                      '(should be fine if you see a whole bunch of numbers/data points...)')
+            # if None in unresolved_counts or None in resolved_counts:
+            #     print(x_points)
+            #     print(y_resolved)
+            #     print(y_unresolved)
+            #     print('There is a None count in here somewhere above, ensure everything is in order!\n'
+            #           '(should be fine if you see a whole bunch of numbers/data points...)')
 
             if ABSOLUTE:
                 # Create two bar plots that are stacked atop each other
@@ -70,7 +77,29 @@ if __name__ == "__main__":
                                          '#punctuation_characters']:
                     plt.xscale('log')
 
-                plt.savefig('./swashplots/' + pickle_file[7:-9] + '.png', dpi=300)
+                plt.savefig('./swashplots/absolutes' + pickle_file[7:-9] + '.png', dpi=300)
+                # plt.show()
+                plt.clf()
+
+            if STACKED:
+                unresolved_bar = plt.bar(x_points, y_unresolved, 0.5, color='fuchsia', bottom=y_resolved,
+                                         align='center')
+                resolved_bar = plt.bar(x_points, y_resolved, 0.5, color='lime', align='center')
+
+                plt.ylabel('#ocurrences')
+                plt.title(pickle_file[:-9])
+                plt.legend((resolved_bar[0], unresolved_bar[1]), ('resolved', 'unresolved'))
+
+                # The #tags graph looks comically bad with log scales,
+                # you might actually want to try it just to see for yourself...
+                if not pickle_file == 'output_#tags_1.pickle':
+                    plt.yscale('log')
+
+                if pickle_file[7:-9] in ['#lines', '#words', 'average_line_length', 'average_word_length',
+                                         '#punctuation_characters']:
+                    plt.xscale('log')
+
+                plt.savefig('./swashplots/stacked' + pickle_file[7:-9] + '.png', dpi=300)
                 # plt.show()
                 plt.clf()
 
@@ -81,6 +110,7 @@ if __name__ == "__main__":
                 resolved_percentages = [float(resolved_dict.get(i, 0)) / j * 100 for i, j in zip(x_points, totals)]
                 unresolved_percentages = [float(unresolved_dict.get(i, 0)) / j * 100 for i, j in zip(x_points, totals)]
 
+                # plt.xlim(xmin=0)
                 unresolved_bar = plt.bar(x_points, unresolved_percentages, 1, color='fuchsia')
                 resolved_bar = plt.bar(x_points, resolved_percentages, 1, color='lime', bottom=unresolved_percentages)
 
@@ -88,4 +118,7 @@ if __name__ == "__main__":
                 plt.title(pickle_file[7:-9])
                 plt.legend((resolved_bar[0], unresolved_bar[0]), ('resolved', 'unresolved'))
 
+                # plt.savefig('./swashplots/percentages' + pickle_file[7:-9] + '.png', dpi=300)
                 plt.show()
+
+                # break
