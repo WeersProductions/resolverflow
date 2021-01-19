f5c58bed964966806f2c2aee447d5e07b7d8f030
Frank Stapel <frank.5voor9@gmail.com>
2021-01-19 11:09:21 +0100

    Vif working!

--- analysis/vif.py
+++ analysis/vif.py
@@ -1,6 +1,8 @@
 """
 Based on: https://github.com/BhaskarBiswas/PySpark-Codes/blob/master/Automated_VIF_Spark.py
 """
+from copy import deepcopy
+
 from pyspark.sql import SparkSession
 
 from pyspark.sql.functions import col
@@ -9,21 +11,15 @@ from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 
 
-def calc_feature_pair_vifs(spark):
+def calc_feature_pair_vifs(spark, features):
     feature_data = spark.read.parquet("/user/***REMOVED***/StackOverflow/output_stackoverflow.parquet")
     feature_data = feature_data.filter(feature_data["is_question"])
-    # Drop columns that are not a feature
-    # feature_data = feature_data.drop("_Id", "_Text", "is_question", "is_answered")
-    # Drop columns that don't correlate with our label
-    # feature_data = feature_data.drop("#lines")
-
-    # For testing
-    feature_data = feature_data.select("#characters", "#punctuation_characters", "punctuation_ratio", "#lines", '#words')
+    feature_data = feature_data.select(*features)
 
     for i in range(len(feature_data.columns) - 1):
         for j in range(i + 1, len(feature_data.columns)):
-            # df = feature_data.rdd.map(lambda x: [Vectors.dense(x[i]), x[j]]).toDF(['feature', 'label'])
-            df = feature_data.select(col(feature_data.columns[i]).alias('raw_feature'), col(feature_data.columns[j]).alias('label'))
+            df = feature_data.select(col(feature_data.columns[i]).alias('raw_feature'),
+                                     col(feature_data.columns[j]).alias('label')).dropna()
             assembler = VectorAssembler(inputCols=['raw_feature'], outputCol='feature', handleInvalid='skip')
             df = assembler.transform(df)
             linear_regression = LinearRegression(featuresCol='feature', labelCol='label')
@@ -32,10 +28,31 @@ def calc_feature_pair_vifs(spark):
             evaluator = RegressionEvaluator(predictionCol='prediction', labelCol='label')
             r_squared = evaluator.evaluate(prediction, {evaluator.metricName: "r2"})
             vif = 1 / (1 - r_squared)
-            print(feature_data.columns[i] + " - " + feature_data.columns[j] + ": " + str(vif))
+            print("pair - " + feature_data.columns[i] + " - " + feature_data.columns[j] + ": " + str(vif))
+
+
+def calc_one_out_vifs(spark, features):
+    feature_data = spark.read.parquet("/user/***REMOVED***/StackOverflow/output_stackoverflow.parquet")
+    feature_data = feature_data.filter(feature_data["is_question"])
+
+    for feature_index, feature in enumerate(features):
+        current_features = deepcopy(features)
+        del current_features[feature_index]
+        df = feature_data.select(*current_features).dropna()
+        assembler = VectorAssembler(inputCols=current_features[:-1], outputCol='feature', handleInvalid='skip')
+        df = assembler.transform(df)
+        linear_regression = LinearRegression(featuresCol='feature', labelCol=current_features[-1])
+        linear_regression_model = linear_regression.fit(df)
+        prediction = linear_regression_model.transform(df)
+        evaluator = RegressionEvaluator(predictionCol='prediction', labelCol=current_features[-1])
+        r_squared = evaluator.evaluate(prediction, {evaluator.metricName: "r2"})
+        vif = 1 / (1 - r_squared)
+        print("without - " + feature + ": " + str(vif))
 
 
 if __name__ == "__main__":
     print("Starting vif analysis.")
-    spark = SparkSession.builder.getOrCreate()
-    calc_feature_pair_vifs(spark)
+    testing_features = ['#characters', '#punctuation_characters', 'punctuation_ratio', '#lines']
+    global_spark = SparkSession.builder.getOrCreate()
+    calc_feature_pair_vifs(global_spark, testing_features)
+    calc_one_out_vifs(global_spark, testing_features)
